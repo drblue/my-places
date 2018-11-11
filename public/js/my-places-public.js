9504b1f1ec665b7205c0d567db71a65db352519f
Johan Nordström <johan@digitalvillage.se>
2018-11-11 23:16:05 +0100

    Tested adding geo-location.

--- public/js/my-places-public.js
+++ public/js/my-places-public.js
@@ -1,36 +1,9 @@
 (function( $ ) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
 	var mymap,
-		mymarkers = [];
+		mymarkers = [],
+		watchID;
 
 	function initMap() {
 		mymap = new google.maps.Map(document.getElementById('my-places-map'), {
@@ -41,7 +14,25 @@
 			zoom: Number(my_places_obj.google_maps_zoom),
 		});
 
+		initGeolocation();
 		addMapMarkers();
+	}
+
+	function initGeolocation() {
+		if ("geolocation" in navigator) {
+			/* geolocation is available */
+			navigator.geolocation.getCurrentPosition(setCurrentPosition);
+			watchID = navigator.geolocation.watchPosition(setCurrentPosition);
+		} else {
+			/* geolocation IS NOT available */
+		}
+	}
+
+	function setCurrentPosition(position) {
+		mymap.setCenter({
+			lat: position.coords.latitude,
+			lng: position.coords.longitude,
+		});
 	}
 
 	function addMapMarkers(placetypes = false) {
